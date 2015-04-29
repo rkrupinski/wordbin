b87636ff2c1654d5978ec5c87f6eedc0cb7a8cc9
Rafał Krupiński <krupinski.rafal@gmail.com>
2015-04-29 20:36:40 +0200

    Added title directive

---
+++ client/js/directives/title.js
@@ -0,0 +1,41 @@
+(function () {
+  'use strict';
+
+  function titleDirective($rootScope, $state, $interpolate, config) {
+    var expr = $interpolate('{{ title }} - {{ app }}');
+
+    function handleStateChange(e, toState) {
+      /*jshint validthis:true*/
+
+      var titleText;
+
+      if (toState.data && toState.data.meta) {
+        titleText = expr(angular.extend({}, toState.data.meta, {
+          app: config.appName
+        }));
+      }
+
+      this.text(titleText || config.appName);
+    }
+
+    return {
+      restrict: 'E',
+      link: function (scope, element) {
+        $rootScope.$on('$stateChangeSuccess',
+            handleStateChange.bind(element));
+      }
+    };
+  }
+
+  titleDirective.$inject = [
+    '$rootScope',
+    '$state',
+    '$interpolate',
+    'config'
+  ];
+
+  angular.module('wordbin')
+
+    .directive('title', titleDirective);
+
+}());
